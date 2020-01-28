f65ad1369f5182596669ea18debddb4202c32aa2
grin-quokka <grinquokka@gmail.com>
2020-01-28 15:25:43 +0900

    :construction: add link to routes

--- src/AddBtnGroup.tsx
+++ src/AddBtnGroup.tsx
@@ -1,19 +1,24 @@
 import React, { ReactElement } from "react";
 import { Button } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
+import { Link } from "react-router-dom";
 
 interface Props {}
 
 export default function AddBtnGroup({}: Props): ReactElement {
   return (
     <div style={{ display: "inline" }}>
-      <Button variant="outlined" color="primary">
-        <AddIcon />
-        시술
-      </Button>
-      <Button variant="outlined" color="secondary">
-        <AddIcon /> 할인
-      </Button>
+      <Link to={"/addtocart/item"}>
+        <Button variant="outlined" color="primary">
+          <AddIcon />
+          시술
+        </Button>
+      </Link>
+      <Link to={"/addtocart/discount"}>
+        <Button variant="outlined" color="secondary">
+          <AddIcon /> 할인
+        </Button>
+      </Link>
     </div>
   );
 }
