b44529a17e5ae675bd6f1955e854b63e4acb42ac
grin-quokka <grinquokka@gmail.com>
2020-01-28 11:17:56 +0900

    :sparkles: add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Cart from "./Cart";
+import AddToCart from "./AddToCart";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
-    <div>
-      <Cart />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Cart} />
+        <Route path="/addtocart/item" exact component={AddToCart} />
+        <Route path="/addtocart/discount" exact component={AddToCart} />
+        <Redirect path="*" to="/" />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
