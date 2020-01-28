c30f8369a85ac50fdaa3a9db5a76a08623fc921a
grin-quokka <grinquokka@gmail.com>
2020-01-28 15:24:51 +0900

    :card_file_box: show menu items from data api

--- src/AddToCart.tsx
+++ src/AddToCart.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
 import {
   Container,
   Paper,
@@ -13,13 +13,25 @@ import { FixedSizeList } from "react-window";
 import CheckIcon from "@material-ui/icons/Check";
 
 interface Props {
-  match: { path: string };
+  mode: string;
+  menu: {
+    items: {
+      [itemKey: string]: { count: number; name: string; price: number };
+    };
+    discounts: { [discountKey: string]: { name: string; rate: number } };
+    currency_code: "string";
+  } | null;
 }
 
-export default function AddToCart({ match: { path } }: Props): ReactElement {
+export default function AddToCart({ mode, menu }: Props): ReactElement {
+  let itemArr: string[] = [];
+  //const [itemArr, setItemArr] = useState<string[]>([]);
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log(event.target.checked);
     console.log(event.target.id);
+    // setItemArr([...itemArr, event.target.id]);
+    itemArr = [...itemArr, event.target.id];
   };
 
   return (
@@ -27,25 +39,35 @@ export default function AddToCart({ match: { path } }: Props): ReactElement {
       <Paper elevation={3}>
         <Toolbar>
           <Typography variant="h6">
-            {path === "/addtocart/item" ? "시술 메뉴" : "할인"}
+            {mode === "item" ? "시술 메뉴" : "할인"}
           </Typography>
         </Toolbar>
         <Divider light />
-        <FixedSizeList height={400} width={"100%"} itemSize={46} itemCount={16}>
-          {({ index, style }) => (
-            <ListItem button style={style} key={index}>
-              <ListItemText primary={`Item ${index + 1}`} secondary={"가격"} />
-              <Checkbox
-                // checked={checked}
-                onChange={handleChange}
-                value="primary"
-                inputProps={{ "aria-label": "Item checkbox" }}
-                id={`${index}`}
-                checkedIcon={<CheckIcon />}
-              />
-            </ListItem>
-          )}
-        </FixedSizeList>
+        {menu !== null && (
+          <FixedSizeList
+            height={500}
+            width={"100%"}
+            itemSize={70}
+            itemCount={Object.keys(menu.items).length}
+          >
+            {({ index, style }) => (
+              <ListItem button style={style} key={index} divider={true}>
+                <ListItemText
+                  primary={menu.items[`i_${index + 1}`].name}
+                  secondary={`${menu.items[`i_${index + 1}`].price}원`}
+                />
+                <Checkbox
+                  onChange={handleChange}
+                  value="primary"
+                  inputProps={{ "aria-label": "Item checkbox" }}
+                  id={`${index}`}
+                  checkedIcon={<CheckIcon />}
+                />
+              </ListItem>
+            )}
+          </FixedSizeList>
+        )}
+
         <Typography variant={"h6"}>완료</Typography>
       </Paper>
     </Container>
