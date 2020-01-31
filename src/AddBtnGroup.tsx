21af3440ae71a46d33f32cff72152c539a286717
grin-quokka <grinquokka@gmail.com>
2020-01-31 10:12:05 +0900

    :lipstick: more space UI for addBtnGroup

--- src/AddBtnGroup.tsx
+++ src/AddBtnGroup.tsx
@@ -1,24 +1,40 @@
 import React, { ReactElement } from "react";
-import { Button } from "@material-ui/core";
+import { Button, Theme, makeStyles, createStyles } from "@material-ui/core";
 import AddIcon from "@material-ui/icons/Add";
 import { Link } from "react-router-dom";
 
-interface Props {}
+const btnBroupStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+      justifyContent: "space-around",
+      marginTop: 10
+    },
+    btnContainer: {
+      width: "40%"
+    }
+  })
+);
 
-export default function AddBtnGroup({}: Props): ReactElement {
+export default function AddBtnGroup(): ReactElement {
+  const classes = btnBroupStyles();
   return (
-    <div style={{ display: "inline" }}>
-      <Link to={"/addtocart/items"}>
-        <Button variant="outlined" color="primary">
-          <AddIcon />
-          시술
-        </Button>
-      </Link>
-      <Link to={"/addtocart/discounts"}>
-        <Button variant="outlined" color="secondary">
-          <AddIcon /> 할인
-        </Button>
-      </Link>
+    <div className={classes.root}>
+      <div className={classes.btnContainer}>
+        <Link to={"/addtocart/items"}>
+          <Button variant="outlined" color="primary" fullWidth>
+            <AddIcon />
+            시술
+          </Button>
+        </Link>
+      </div>
+      <div className={classes.btnContainer}>
+        <Link to={"/addtocart/discounts"}>
+          <Button variant="outlined" color="secondary" fullWidth>
+            <AddIcon /> 할인
+          </Button>
+        </Link>
+      </div>
     </div>
   );
 }
